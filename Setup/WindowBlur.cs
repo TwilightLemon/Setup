37034e43cd1e3cd8eecbfb2fd78ab3bdfa0b37f8
TwilightLemon <cz241126@live.com>
2020-02-08 18:50:13 +0800

    Release[1.1.3.6]

---
+++ Setup/WindowBlur.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+using System.Windows.Media;
+
+namespace Setup
+{
+    /// <summary>
+    /// 为窗口提供模糊特效。
+    /// </summary>
+    public class WindowAccentCompositor
+    {
+        private readonly Window _window;
+        private bool _isEnabled;
+        private int _blurColor;
+
+        /// <summary>
+        /// 创建 <see cref="WindowAccentCompositor"/> 的一个新实例。
+        /// </summary>
+        /// <param name="window">要创建模糊特效的窗口实例。</param>
+        public WindowAccentCompositor(Window window) => _window = window ?? throw new ArgumentNullException(nameof(window));
+
+        /// <summary>
+        /// 获取或设置此窗口模糊特效是否生效的一个状态。
+        /// 默认为 false，即不生效。
+        /// </summary>
+        [DefaultValue(false)]
+        public bool IsEnabled
+        {
+            get => _isEnabled;
+            set
+            {
+                _isEnabled = value;
+                OnIsEnabledChanged(value);
+            }
+        }
+        private Color c;
+        /// <summary>
+        /// 获取或设置此窗口模糊特效叠加的颜色。
+        /// </summary>
+        public Color Color
+        {
+            get => Color.FromArgb(
+                // 取出红色分量。
+                (byte)((_blurColor & 0x000000ff) >> 0),
+                // 取出绿色分量。
+                (byte)((_blurColor & 0x0000ff00) >> 8),
+                // 取出蓝色分量。
+                (byte)((_blurColor & 0x00ff0000) >> 16),
+                // 取出透明分量。
+                (byte)((_blurColor & 0xff000000) >> 24));
+            set
+            {
+                c = value;
+                _blurColor =
+               // 组装红色分量。
+               value.R << 0 |
+               // 组装绿色分量。
+               value.G << 8 |
+               // 组装蓝色分量。
+               value.B << 16 |
+               // 组装透明分量。
+               value.A << 24;
+            }
+        }
+
+        private void OnIsEnabledChanged(bool isEnabled)
+        {
+            Window window = _window;
+            var handle = new WindowInteropHelper(window).EnsureHandle();
+            Composite(handle, isEnabled);
+        }
+
+        private void Composite(IntPtr handle, bool isEnabled)
+        {
+            var accent = new AccentPolicy();
+            if (!isEnabled)
+                accent.AccentState = AccentState.ACCENT_DISABLED;
+            else{
+                accent.AccentState = AccentState.ACCENT_ENABLE_ACRYLICBLURBEHIND;
+                accent.GradientColor = _blurColor;
+            }
+            // 将托管结构转换为非托管对象。
+            var accentPolicySize = Marshal.SizeOf(accent);
+            var accentPtr = Marshal.AllocHGlobal(accentPolicySize);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            // 设置窗口组合特性。
+            try
+            {
+                // 设置模糊特效。
+                var data = new WindowCompositionAttributeData
+                {
+                    Attribute = WindowCompositionAttribute.WCA_ACCENT_POLICY,
+                    SizeOfData = accentPolicySize,
+                    Data = accentPtr,
+                };
+                SetWindowCompositionAttribute(handle, ref data);
+            }
+            finally
+            {
+                // 释放非托管对象。
+                Marshal.FreeHGlobal(accentPtr);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        private static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+
+        private enum AccentState
+        {
+            /// <summary>
+            /// 完全禁用 DWM 的叠加特效。
+            /// </summary>
+            ACCENT_DISABLED = 0,
+
+            /// <summary>
+            /// 
+            /// </summary>
+            ACCENT_ENABLE_GRADIENT = 1,
+            ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+            ACCENT_ENABLE_BLURBEHIND = 3,
+            ACCENT_ENABLE_ACRYLICBLURBEHIND = 4,
+            ACCENT_INVALID_STATE = 5,
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct AccentPolicy
+        {
+            public AccentState AccentState;
+            public int AccentFlags;
+            public int GradientColor;
+            public int AnimationId;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct WindowCompositionAttributeData
+        {
+            public WindowCompositionAttribute Attribute;
+            public IntPtr Data;
+            public int SizeOfData;
+        }
+
+        private enum WindowCompositionAttribute
+        {
+            // 省略其他未使用的字段
+            WCA_ACCENT_POLICY = 19,
+            // 省略其他未使用的字段
+        }
+
+        /// <summary>
+        /// 当前操作系统支持的透明模糊特效级别。
+        /// </summary>
+        public enum BlurSupportedLevel
+        {
+            /// <summary>
+            /// 
+            /// </summary>
+            NotSupported,
+            Aero,
+            Blur,
+            Acrylic,
+        }
+    }
+}
---
+++ Uninstall/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uninstall
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                if (MessageBox.Show("您真的要忍心卸载Lemon App吗?/(ㄒoㄒ)/~~", "Lemon App卸载程序", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                {
+                    Process[] processList = Process.GetProcesses();
+                    foreach (Process process in processList)
+                    {
+                        if (process.ProcessName == "LemonApp")
+                            process.Kill();
+                    }
+                    await Task.Delay(2000);
+                    try { new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu) + @"\Lemon App").Delete(true); } catch { }
+                    try { File.Delete(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + @"\Lemon App.lnk"); } catch { }
+
+                    var a = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+                    var list = a.GetFiles();
+                    foreach (var file in list)
+                    {
+                        try
+                        {
+                            if (file.Name != "uninstall.exe")
+                                file.Delete();
+                        }
+                        catch { }
+                    }
+                    try
+                    {
+                        RegistryKey hklm = Registry.LocalMachine;
+                        hklm.DeleteSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\LemonApp\", false);
+                        hklm.Close();
+                    }
+                    catch { }
+                    MessageBox.Show("卸载成功！o(*￣▽￣*)ブ", "Lemon App卸载程序");
+                }
+                else MessageBox.Show("感谢您！让我为您继续服务吧！o(*￣▽￣*)ブ", "开心");
+            }
+            catch { MessageBox.Show("卸载失败惹！o(*￣▽￣*)ブ", "Lemon App卸载程序"); }
+        }
+    }
+}
