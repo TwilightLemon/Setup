bab81f82adc1740abb588f6e8a5695fff3e25c33
TwilightLemon <cz241126@live.com>
2023-11-03 10:56:41 +0800

    让Copilot帮忙写点注释

--- Setup/MainWindow.xaml.cs
+++ Setup/MainWindow.xaml.cs
@@ -16,34 +16,94 @@ namespace Setup
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region 安装信息
         static string AppName = "Lemon App";
         static string AppFileName = "LemonApp";
-        static string BuildVersion = "1.2.7.0";
+        static string BuildVersion = "1.2.7.4";
         static string Publisher = "Twilight./Lemon";
         static string SignText = "Powered by .NET6";
+        static string DefaultPath = "C:\\Program Files\\"+AppFileName;
+        #endregion
 
+        private Storyboard StartAni,CloseAni;
+
+        #region Window基本实现
         public MainWindow()
         {
+            #region 初始化窗口
             InitializeComponent();
+            MouseLeftButtonDown += (sender, e) => {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                    DragMove();
+            };
+            Title = AppName + "安装程序";
+            TitleRun.Text = AppName;
+            signe.Text = SignText;
+            doingText.Text = "";
+            #endregion
+            #region 关闭正在运行的程序
             Process[] processList = Process.GetProcesses();
             foreach (Process process in processList)
             {
                 if (process.ProcessName == AppFileName)
                     process.Kill();
             }
-            MouseLeftButtonDown += (sender,e)=>{
-                if (e.LeftButton == MouseButtonState.Pressed)
-                    DragMove();
-            };
-            (Resources["Start"] as Storyboard).Completed += delegate {
+            #endregion
+
+            //载入动画资源
+            CloseAni = Resources["Finish"] as Storyboard;
+            StartAni = Resources["Start"] as Storyboard;
+            StartAni.Completed += delegate {
                 Thread v = new Thread(Setup);
                 v.Start(new {path=path.Text,istb= checkBox.IsChecked});
             };
+        }
 
-            Title = AppName + "安装程序";
-            TitleRun.Text = AppName;
-            signe.Text = SignText;
-            doingText.Text = "";
+        private void window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string LastPath = "";
+            try
+            {
+                //查找已经安装的信息
+                RegistryKey hklm = Registry.LocalMachine;
+                RegistryKey hkSoftWare = hklm.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\" + AppFileName);
+                LastPath = hkSoftWare.GetValue("InstallLocation").ToString();
+                hklm.Close();
+                hkSoftWare.Close();
+            }
+            finally
+            {
+                path.Text = !string.IsNullOrEmpty(LastPath) ? LastPath : DefaultPath;
+            }
+        }
+
+        private void FinishBtn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var ps = path.Text + "\\" + AppFileName + ".exe";
+            CloseAni.Completed += async delegate {
+                await Task.Delay(100);
+                Process.Start(ps);
+                Environment.Exit(0);
+            };
+            CloseAni.Begin();
+        }
+
+        private void closeBtn_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            CloseAni.Completed += async delegate {
+                await Task.Delay(100);
+                Environment.Exit(0);
+            };
+            CloseAni.Begin();
+        }
+        #endregion
+        #region 安装实现
+        private void pro_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //安装进度条
+            if (e.NewValue == 100)
+                //安装完成
+                (Resources["OK"] as Storyboard).Begin();
         }
         private void SetupDoing(string text) {
             Dispatcher.Invoke(() => {
@@ -82,6 +142,7 @@ namespace Setup
             //创建开始菜单卸载 快捷方式
             ShortcutCreator.CreateShortcut(Environment.GetFolderPath(Environment.SpecialFolder.StartMenu) + @"\Programs" + @"\"+AppName+@"\", "卸载"+AppName, xt + @"\Uninstall.exe", null, xt + @"\Uninstall.exe");
          
+            //创建注册表
             RegistryKey hklm = Registry.LocalMachine;
             RegistryKey hkSoftWare = hklm.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\"+AppFileName);
             hkSoftWare.SetValue("DisplayIcon", xt + @"\"+AppFileName+".exe", RegistryValueKind.String);
@@ -96,6 +157,7 @@ namespace Setup
         }
         private void ChooseSetupPath_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            //选择安装路径
             var fbd = new System.Windows.Forms.FolderBrowserDialog();
             if (fbd.ShowDialog() != System.Windows.Forms.DialogResult.Cancel)
             {
@@ -104,46 +166,6 @@ namespace Setup
                     path.Text = Path.Combine(path.Text,AppName);
             }
         }
-
-        private void pro_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (e.NewValue == 100)
-                (Resources["OK"] as Storyboard).Begin();
-        }
-
-        private void FinishBtn_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            var ps = path.Text + "\\"+AppFileName+".exe";
-            var close = Resources["Finish"] as Storyboard;
-            close.Completed += async delegate {
-                await Task.Delay(100);
-                Process.Start(ps);
-                Environment.Exit(0);
-            };
-            close.Begin();
-        }
-
-        private void window_Loaded(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                RegistryKey hklm = Registry.LocalMachine;
-                RegistryKey hkSoftWare = hklm.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\" + AppFileName);
-                string LastPath = hkSoftWare.GetValue("InstallLocation").ToString();
-                hklm.Close();
-                hkSoftWare.Close();
-                path.Text = !string.IsNullOrEmpty(LastPath)?LastPath: "C:\\Program Files\\Lemon App";
-            }
-            catch { }
-        }
-        private void closeBtn_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            var close = Resources["Finish"] as Storyboard;
-            close.Completed += async delegate {
-                await Task.Delay(100);
-                Environment.Exit(0);
-            };
-            close.Begin();
-        }
+        #endregion
     }
 }
